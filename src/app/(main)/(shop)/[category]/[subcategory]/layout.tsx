19f0346f3011c9df8eb857d2d13994cf9008b481
sebasRT <rtsebas11@gmail.com>
2025-01-19 16:41:04 -0500

    fix subcategories selector to map over categories directly instead of using category-specific subcategories

--- src/app/(main)/(shop)/[category]/[subcategory]/layout.tsx
+++ src/app/(main)/(shop)/[category]/[subcategory]/layout.tsx
@@ -35,7 +35,7 @@ const SubcategoriesSelector = ({ category, subcategory }: { category: Category, 
             <div className='absolute inset-0 hidden peer-focus:block'></div>
             <ul className='text-lg max-h-0 peer-focus:max-h-[30rem] hover:max-h-[30rem] overflow-y-clip transition-[max-height_1s_ease-in-out] absolute z-50 bg-white shadow-md right-0 text-right'>
 
-                {categories[category].map((value, key) => (
+                {categories.map((value, key) => (
                     <li key={key} value={value} className=' p-2 odd:bg-slate-50 even:bg-slate-100 '>
                         <Link href={`/${category}/${value}`} className='hover:text-blue-500' replace>{formatName(value)}</Link>
                     </li>
